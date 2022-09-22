733afc6e0247e500d8aca222479554c6d1f46331
Connor <kor.connor@gmail.com>
2022-09-23 01:54:25 +0900

    안쓰지만 알아야 하는 함수형 컴포넌트

---
+++ index.js
@@ -0,0 +1,15 @@
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+// 함수형 컴포넌트
+function helloWorldButton () {
+    return React.createElement(
+        "button", 
+        {onclick: () => {
+            
+        }},
+        "Hello world!"
+        )
+}
+
+
