ec1b68d3ff4ccc39ff1d77c68136a0ff2f88a4b8
Connor <kor.connor@gmail.com>
2022-09-23 02:04:43 +0900

    비구조화 할당

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 
 // 함수형 컴포넌트
 function helloWorldButton () {
+
+    // 비 구조화 할당
+    const [isClick, setClickState] = React.useState(false)
+    
     return React.createElement(
         "button", 
         {onclick: () => {
@@ -14,3 +18,10 @@ function helloWorldButton () {
 
 const rootContainer = document.getElementById('react-root')
 ReactDOM.render(React.createElement(helloWorldButton), rootContainer)
+
+function returnOneThreeArray() {
+    return [1, 3]
+}
+
+const [a, b] = returnOneThreeArray()
+const [a, b] = [1, 3]
